579d9772dda79525ba9a72ce3a94ca58b1ece596
Teena <teenaholee03@gmail.com>
2026-04-07 21:30:03 +0530

    feat: add resource repository and advanced filtering specification

--- smart-campus-api/src/main/java/com/smartcampus/resource/repository/ResourceRepository.java
+++ smart-campus-api/src/main/java/com/smartcampus/resource/repository/ResourceRepository.java
@@ -1,5 +1,12 @@
 package com.smartcampus.resource.repository;
 
-public class ResourceRepository {
-    
+import com.smartcampus.resource.entity.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ResourceRepository extends JpaRepository<Resource, Long>, JpaSpecificationExecutor<Resource> {
+
+    boolean existsByResourceCode(String resourceCode);
+
+    boolean existsByResourceCodeAndIdNot(String resourceCode, Long id);
 }
