50103d2888c7910e4c3124c6926a57c466db1c05
Teena <teenaholee03@gmail.com>
2026-04-08 09:20:59 +0530

    feat: add resource exceptions and global API error handling

--- smart-campus-api/src/main/java/com/smartcampus/resource/exception/DuplicateResourceCodeException.java
+++ smart-campus-api/src/main/java/com/smartcampus/resource/exception/DuplicateResourceCodeException.java
@@ -1,6 +1,7 @@
 package com.smartcampus.resource.exception;
 
 public class DuplicateResourceCodeException extends RuntimeException {
+
     public DuplicateResourceCodeException(String message) {
         super(message);
     }
--- smart-campus-api/src/main/java/com/smartcampus/resource/exception/ResourceNotFoundException.java
+++ smart-campus-api/src/main/java/com/smartcampus/resource/exception/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.smartcampus.resource.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
