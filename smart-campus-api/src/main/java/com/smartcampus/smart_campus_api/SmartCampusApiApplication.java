94b29681fc5afb6843ae747064cb2f66d38dd19a
leshakamadara <leshakamadara@gmail.com>
2026-04-17 17:26:00 +0530

    chore(config): wire notification module security and app scanning

--- smart-campus-api/src/main/java/com/smartcampus/smart_campus_api/SmartCampusApiApplication.java
+++ smart-campus-api/src/main/java/com/smartcampus/smart_campus_api/SmartCampusApiApplication.java
@@ -2,8 +2,10 @@ package com.smartcampus.smart_campus_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.smartcampus")
+@EnableJpaRepositories(basePackages = "com.smartcampus")
 public class SmartCampusApiApplication {
 
 	public static void main(String[] args) {
