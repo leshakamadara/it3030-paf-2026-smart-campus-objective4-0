45632f45ca38b7e5dec0ba18fd1efe413a505a33
leshakamadara <leshakamadara@gmail.com>
2026-04-26 02:01:00 +0530

    fix: correct mismatched div tag in AdminTicketDetailView.tsx

--- smart-campus-client/src/pages/module-c-maintenance-incident-ticketing/AdminTicketDetailView.tsx
+++ smart-campus-client/src/pages/module-c-maintenance-incident-ticketing/AdminTicketDetailView.tsx
@@ -475,7 +475,6 @@ export const AdminTicketDetailView = ({
                       </button>
                     )}
                   </div>
-                  </div>
                 </div>
               )}
 
